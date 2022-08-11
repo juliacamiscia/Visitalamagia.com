6b42d498f15c7724b4e8c33f22723cec2de0711a
Julia Camiscia <julicamiscia@gmail.com>
2022-08-10 22:15:27 -0300

    Segunda Entrega

--- js/dom.js
+++ js/dom.js
@@ -1,15 +1,3 @@
-// let nombreIngresado = prompt("Ingrese su nombre:");
-// let apellidoIngresado = prompt("Ingrese su apellido:");
-// let edad = parseInt(prompt("Ingrese su edad:"));
-
-// if((nombreIngresado !="") && (apellidoIngresado !="")){
-//     alert("¡ Bienvenido/a " + nombreIngresado + " " + apellidoIngresado + " !");
-// }
-
-// else{
-//     alert("Error: Ingresar nombre y apellido")
-// }
-
 function Destino (nombre, precio, imagen){
     this.nombre = nombre;
     this.precio = precio;
@@ -28,17 +16,30 @@ function calculoPrecio (cantidad, precio){
     precioTotal += cantidad * precio
 }
 
-let edad = parseInt(prompt("Ingrese su edad:"));
+let inputEdad =document.getElementById("edad").value;
 
-if(edad >= 18){
-    alert("Sos mayor de edad, podés pedir presupuesto para tu viaje");
+if(inputEdad>=18){
     
+    let card = document.getElementById("div1");
+    for(const destino of listaDestinos){
+        let laCard = document.createElement("div");
+        laCard.style.backgroundColor="#E9C8EC";
+        laCard.style.textAlign="center";
+        laCard.style.margin="50px";
+
+        laCard.innerHTML += `<h3>${destino.nombre}</h3>
+                        <p> $${destino.precio} por día</p>
+                        <img src=${destino.imagen} width="450px" height="280px" alt="imagenDestino"</img>`
+
+        card.append(laCard);
+    }
+
     let cantidadLugares = parseInt (prompt("Ingrese la cantidad de lugares que desea visitar: \n-" + nombresDestinos.join("\n-")))
 
     for(let i = 0; i < cantidadLugares ; i++){
 
         let presupuesto1 = prompt("Ingrese el nombre del lugar que quieres visitar: \n-" + nombresDestinos.join("\n-"));
-    
+
         if(presupuesto1 == "Disney"){
             alert("Genial!! Elegiste como destino: " + presupuesto1);
             let cantidad1 = parseInt (prompt("Ingrese la cantidad de parques que quieres visitar:"));
@@ -55,38 +56,72 @@ if(edad >= 18){
 
         else{
             alert ("Ingrese un nombre válido");
-        } 
+        }
     }
-    alert("Este es el precio total de tu presupuesto: $" + precioTotal + ". Por otras consultas contactate con nosotros!")  
+    alert("Este es el precio total de tu presupuesto: $" + precioTotal + ". Por otras consultas contactate con nosotros!")
+}
+
+else if(inputEdad < 18){
+    let card2 = document.getElementById("div2");
+    let mensajeMenores = document.createElement("div");
+    mensajeMenores.style.backgroundColor="#E9C8EC";
+    mensajeMenores.style.textAlign="center";
+    mensajeMenores.style.margin="50px";
+
+    mensajeMenores.innerHTML += `<h2>No sos mayor de edad, deberás pedir ayuda a un adulto para pedir un presupuesto</h2>
+                    <img src="../images/minnietriste.gif" width="300px" height="200px" alt="imagenDestino"</img>`
+
+    card2.append(mensajeMenores);
 }
 
 else{
-    alert("No sos mayor de edad, deberás pedir ayuda a un adulto para pedir un presupuesto")
+    laCard.remove();
+    mensajeMenores.remove();
 }
 
-let card = document.getElementById("div1");
-for(const destino of listaDestinos){
-    let laCard = document.createElement("div");
-    laCard.style.backgroundColor="#E9C8EC";
-    laCard.style.textAlign="center";
-    laCard.style.margin="50px";
-
-    laCard.innerHTML += `<h3>${destino.nombre}</h3>
-                    <p> $${destino.precio} por día</p>
-                    <img src=${destino.imagen} width="450px" height="280px" alt="imagenDestino"</img>`
-
-    card.append(laCard);
-
-    if(edad < 18){
-        laCard.remove();
-    }
-} 
-
-let formulario = document.getElementById("form")
+// let formulario = document.getElementById("form")
+let form = document.getElementById('form');
+// let formulario = form.value;
 
 function validateform(e){
-    e.preventDefault()
-    console.log("¡Tu formulario fue enviado con éxito!")
-}  
+    e.preventDefault();
+    console.log("¡Tu formulario fue enviado con éxito!");
+}
 
 form.addEventListener("submit", validateform);
+
+
+let botonGuardado = document.getElementById("botonGuardado");
+botonGuardado.addEventListener("click", saveForm);
+
+
+let respuestas = document.getElementsByClassName("respuestas");
+
+function saveForm(){
+
+    let toSave = [];
+    
+    for(let i = 0; i < respuestas; i++){
+        let toSave1 = respuestas(i)
+    
+        let toSaveInfo = {
+            task: toSave1.innerText,
+        }
+    
+        toSave.push(toSaveInfo)
+    }
+    
+    localStorage.setItem("formularioGuardado", JSON.stringify(toSave))
+}
+
+function loadForm(){
+
+    let formularioGuardado = JSON.parse(localStorage.getItem("formularioGuardado"))
+    
+    for(let i = 0; i < respuestas.length; i++)
+        console.log(respuestas[i].value);
+    }
+
+    loadForm()
+
+    console.log(respuestas)
